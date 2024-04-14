f9bc0431cc1225e7256c16b116cde5534eb66940
ebokyung <dlqhrud303@naver.com>
2024-04-14 22:17:18 +0900

    chore: 프리티어 설정

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid', // 화살표 함수의 매개변수가 하나일 때 괄호 생략 여부 (생략할 경우 aviod)
+  endOfLine: 'lf', // 개행문자 설정
+  printWidth: 120, // 코드 한 줄의 max lnegth 120
+  semi: true, // 세미콜론 사용 여부
+  singleQuote: true, // 홑따옴표 사용
+  tabWidth: 2, // 탭의 너비 2칸
+  trailingComma: 'all', // key: value 뒤에 항상 콤마 붙이기
+  useTabs: false, // 탭 사용 여부
+};
