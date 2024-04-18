6ba935834dfd5faf341b07d857215ea2ac32ac8c
ebokyung <dlqhrud303@naver.com>
2024-04-18 17:20:27 +0900

    feat: button 스토리북 작성

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -13,7 +13,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const SolidLarge: Story = {
+export const SolidLargePrimary: Story = {
   args: {
     children: 'Button',
     variant: 'solid',
@@ -21,17 +21,71 @@ export const SolidLarge: Story = {
   },
 };
 
-export const OutlineMedium: Story = {
+export const SolidLargeSecondary: Story = {
   args: {
     children: 'Button',
-    variant: 'outline',
+    variant: 'solid',
+    size: 'large',
+    color: 'secondary',
   },
 };
 
-export const GhostSmall: Story = {
+export const SolidLargeNegative: Story = {
+  args: {
+    children: 'Button',
+    variant: 'solid',
+    size: 'large',
+    color: 'negative',
+  },
+};
+
+export const OutlineMediumPrimary: Story = {
+  args: {
+    children: 'Button',
+    variant: 'outline',
+    color: 'primary',
+  },
+};
+
+export const OutlineMediumSecondary: Story = {
+  args: {
+    children: 'Button',
+    variant: 'outline',
+    color: 'secondary',
+  },
+};
+
+export const OutlineMediumNegative: Story = {
+  args: {
+    children: 'Button',
+    variant: 'outline',
+    color: 'negative',
+  },
+};
+
+export const GhostSmallPrimary: Story = {
   args: {
     children: 'Button',
     variant: 'ghost',
     size: 'small',
+    color: 'primary',
+  },
+};
+
+export const GhostSmallSecondary: Story = {
+  args: {
+    children: 'Button',
+    variant: 'ghost',
+    size: 'small',
+    color: 'secondary',
+  },
+};
+
+export const GhostSmallNegative: Story = {
+  args: {
+    children: 'Button',
+    variant: 'ghost',
+    size: 'small',
+    color: 'negative',
   },
 };
